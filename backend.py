a1f4dfbd2436762d148b3a442be4d43c156a2d65
dodo <dodo@chaosdorf.de>
2017-06-27 22:19:46 +0200

    weitergearbeitet

--- backend.py
+++ backend.py
@@ -1,8 +1,29 @@
+import random
+import list_gen
+
+project_filename = "Projektauswahl.txt"
+language_filename = "Programmiersprachen.txt"
+constraint_filename = "constraints.txt"
+
+
 def get_project():
-    return "project foo"
+    project_list = list_gen.lines_from_file(project_filename)
+    project = random.choice(project_list)
+    return project
 
 def get_language():
-    return "language foo"
+    language_list = list_gen.lines_from_file(language_filename)
+    language = random.choice(language_list)
+    return language
 
 def get_constraint():
-    return "constraint foo"
+    constraint_list = list_gen.lines_from_file(constraint_filename)
+    constraint = random.choice(constraint_list)
+    return constraint
+
+def get_job(project, language, constraint):
+    project = project.strip("\n")
+    language = language.strip("\n")
+    constraint = constraint.strip("\n")
+    result = "Schreibe {0} in {1} {2}.".format(project, language, constraint)
+    return result
